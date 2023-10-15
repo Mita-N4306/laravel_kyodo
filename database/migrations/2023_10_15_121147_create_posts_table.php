f141d6fa0ecf15452e75f84f687345a24327ad10
naoki mitamura <zango3333@yahoo.co.jp>
2023-10-15 12:31:06 +0900

    投稿テーブルのマイグレーションとシーダー作成(2023.10.15)

---
+++ database/migrations/2023_10_15_121147_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title'); //タイトルカラム追加
+            $table->text('body');    //投稿内容カラム追加
+            $table->text('image')->nullable();  //画像カラム追加(nullable()....画像挿入は必須ではないために使うメソッド)
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
